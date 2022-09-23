9d70384d0c259a424def11f7bb2bf5823d3fd5e0
Briusx <79315445+Briusx@users.noreply.github.com>
2022-09-22 23:17:58 -0500

    pt 1

---
+++ app/src/main/java/exam1/ExamenProg2.java
@@ -0,0 +1,16 @@
+package exam1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.anahuac.anahuac.R;
+
+public class ExamenProg2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_examen_prog2);
+    }
+}
